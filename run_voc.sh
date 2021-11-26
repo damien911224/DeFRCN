50d2d1997cd8f7a5b8a61a5630ae19145889bec8
damien911224 <damien911224@gmail.com>
2021-11-26 21:29:09 +0900

    Modified

--- run_voc.sh
+++ run_voc.sh
@@ -8,7 +8,7 @@ SPLIT_ID=$2
 
 
 # ------------------------------- Base Pre-train ---------------------------------- #
-python3 main.py --num-gpus 8 --config-file configs/voc/defrcn_det_r101_base${SPLIT_ID}.yaml     \
+python3 main.py --num-gpus 2 --config-file configs/voc/defrcn_det_r101_base${SPLIT_ID}.yaml     \
     --opts MODEL.WEIGHTS ${IMAGENET_PRETRAIN}                                                   \
            OUTPUT_DIR ${SAVE_DIR}/defrcn_det_r101_base${SPLIT_ID}
 
@@ -31,7 +31,7 @@ do
                 --shot ${shot} --seed ${seed} --setting 'fsod' --split ${SPLIT_ID}
             CONFIG_PATH=configs/voc/defrcn_fsod_r101_novel${SPLIT_ID}_${shot}shot_seed${seed}.yaml
             OUTPUT_DIR=${SAVE_DIR}/defrcn_fsod_r101_novel${SPLIT_ID}/fsrw-like/${shot}shot_seed${seed}_repeat${repeat_id}
-            python3 main.py --num-gpus 8 --config-file ${CONFIG_PATH}                          \
+            python3 main.py --num-gpus 2 --config-file ${CONFIG_PATH}                          \
                 --opts MODEL.WEIGHTS ${BASE_WEIGHT} OUTPUT_DIR ${OUTPUT_DIR}                   \
                        TEST.PCB_MODELPATH ${IMAGENET_PRETRAIN_TORCH}
             rm ${CONFIG_PATH}
@@ -59,7 +59,7 @@ do
             --shot ${shot} --seed ${seed} --setting 'gfsod' --split ${SPLIT_ID}
         CONFIG_PATH=configs/voc/defrcn_gfsod_r101_novel${SPLIT_ID}_${shot}shot_seed${seed}.yaml
         OUTPUT_DIR=${SAVE_DIR}/defrcn_gfsod_r101_novel${SPLIT_ID}/tfa-like/${shot}shot_seed${seed}
-        python3 main.py --num-gpus 8 --config-file ${CONFIG_PATH}                            \
+        python3 main.py --num-gpus 2 --config-file ${CONFIG_PATH}                            \
             --opts MODEL.WEIGHTS ${BASE_WEIGHT} OUTPUT_DIR ${OUTPUT_DIR}                     \
                    TEST.PCB_MODELPATH ${IMAGENET_PRETRAIN_TORCH}
         rm ${CONFIG_PATH}
@@ -80,7 +80,7 @@ do
             --shot ${shot} --seed ${seed} --setting 'fsod' --split ${SPLIT_ID}
         CONFIG_PATH=configs/voc/defrcn_fsod_r101_novel${SPLIT_ID}_${shot}shot_seed${seed}.yaml
         OUTPUT_DIR=${SAVE_DIR}/defrcn_fsod_r101_novel${SPLIT_ID}/tfa-like/${shot}shot_seed${seed}
-        python3 main.py --num-gpus 8 --config-file ${CONFIG_PATH}                             \
+        python3 main.py --num-gpus 2 --config-file ${CONFIG_PATH}                             \
             --opts MODEL.WEIGHTS ${BASE_WEIGHT} OUTPUT_DIR ${OUTPUT_DIR}                      \
                    TEST.PCB_MODELPATH ${IMAGENET_PRETRAIN_TORCH}
         rm ${CONFIG_PATH}
