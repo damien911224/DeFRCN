0e8ca247e8367942f28df08d962de3a30f31ded9
damien911224 <damien911224@gmail.com>
2021-12-07 00:34:03 +0900

    Modified

--- run_coco.sh
+++ run_coco.sh
@@ -7,39 +7,39 @@ IMAGENET_PRETRAIN_TORCH=pre-trained/ImageNetPretrained/torchvision/resnet101-5d3
 
 
 # ------------------------------- Base Pre-train ---------------------------------- #
-#python3 main.py --num-gpus 2 --config-file configs/coco/defrcn_det_r101_base.yaml     \
-#    --opts MODEL.WEIGHTS ${IMAGENET_PRETRAIN}                                         \
-#           OUTPUT_DIR ${SAVEDIR}/defrcn_det_r101_base
+python3 main.py --num-gpus 2 --config-file configs/coco/defrcn_det_r101_base.yaml     \
+    --opts MODEL.WEIGHTS ${IMAGENET_PRETRAIN}                                         \
+           OUTPUT_DIR ${SAVEDIR}/defrcn_det_r101_base
 
 
 # ------------------------------ Model Preparation -------------------------------- #
-#python3 tools/model_surgery.py --dataset coco --method remove                         \
-#    --src-path ${SAVEDIR}/defrcn_det_r101_base/model_final.pth                        \
-#    --save-dir ${SAVEDIR}/defrcn_det_r101_base
-#BASE_WEIGHT=${SAVEDIR}/defrcn_det_r101_base/model_reset_remove.pth
+python3 tools/model_surgery.py --dataset coco --method remove                         \
+    --src-path ${SAVEDIR}/defrcn_det_r101_base/model_final.pth                        \
+    --save-dir ${SAVEDIR}/defrcn_det_r101_base
+BASE_WEIGHT=${SAVEDIR}/defrcn_det_r101_base/model_reset_remove.pth
 
 
 # ------------------------------ Novel Fine-tuning -------------------------------- #
 # --> 1. FSRW-like, i.e. run seed0 10 times (the FSOD results on coco in most papers)
-#for repeat_id in 0 1 2 3 4 5 6 7 8 9
-#do
-#    for shot in 1 2 3 5 10 30
-#    do
-#        for seed in 0
-#        do
-#            python3 tools/create_config.py --dataset coco14 --config_root configs/coco \
-#                --shot ${shot} --seed ${seed} --setting 'fsod'
-#            CONFIG_PATH=configs/coco/defrcn_fsod_r101_novel_${shot}shot_seed${seed}.yaml
-#            OUTPUT_DIR=${SAVEDIR}/defrcn_fsod_r101_novel/fsrw-like/${shot}shot_seed${seed}_repeat${repeat_id}
-#            python3 main.py --num-gpus 2 --config-file ${CONFIG_PATH}                  \
-#                --opts MODEL.WEIGHTS ${BASE_WEIGHT} OUTPUT_DIR ${OUTPUT_DIR}           \
-#                       TEST.PCB_MODELPATH ${IMAGENET_PRETRAIN_TORCH}
-#            rm ${CONFIG_PATH}
-#            rm ${OUTPUT_DIR}/model_final.pth
-#        done
-#    done
-#done
-#python3 tools/extract_results.py --res-dir ${SAVEDIR}/defrcn_fsod_r101_novel/fsrw-like --shot-list 1 2 3 5 10 30  # surmarize all results
+for repeat_id in 0 1 2 3 4 5 6 7 8 9
+do
+    for shot in 1 2 3 5 10 30
+    do
+        for seed in 0
+        do
+            python3 tools/create_config.py --dataset coco14 --config_root configs/coco \
+                --shot ${shot} --seed ${seed} --setting 'fsod'
+            CONFIG_PATH=configs/coco/defrcn_fsod_r101_novel_${shot}shot_seed${seed}.yaml
+            OUTPUT_DIR=${SAVEDIR}/defrcn_fsod_r101_novel/fsrw-like/${shot}shot_seed${seed}_repeat${repeat_id}
+            python3 main.py --num-gpus 2 --config-file ${CONFIG_PATH}                  \
+                --opts MODEL.WEIGHTS ${BASE_WEIGHT} OUTPUT_DIR ${OUTPUT_DIR}           \
+                       TEST.PCB_MODELPATH ${IMAGENET_PRETRAIN_TORCH}
+            rm ${CONFIG_PATH}
+            rm ${OUTPUT_DIR}/model_final.pth
+        done
+    done
+done
+python3 tools/extract_results.py --res-dir ${SAVEDIR}/defrcn_fsod_r101_novel/fsrw-like --shot-list 1 2 3 5 10 30  # surmarize all results
 
 
 # ----------------------------- Model Preparation --------------------------------- #
@@ -51,8 +51,8 @@ BASE_WEIGHT=${SAVEDIR}/defrcn_det_r101_base/model_reset_surgery.pth
 
 # ------------------------------ Novel Fine-tuning ------------------------------- #
 # --> 2. TFA-like, i.e. run seed0~9 for robust results (G-FSOD, 80 classes)
-#for seed in 0 1 2 3 4 5 6 7 8 9
-for seed in 0
+for seed in 0 1 2 3 4 5 6 7 8 9
+#for seed in 0
 do
     for shot in 1 2 3 5 10 30
     do
@@ -73,8 +73,8 @@ python3 tools/extract_results.py --res-dir ${SAVEDIR}/defrcn_gfsod_r101_novel/tf
 # ------------------------------ Novel Fine-tuning ------------------------------- #  not necessary, just for the completeness of defrcn
 # --> 3. TFA-like, i.e. run seed0~9 for robust results
 BASE_WEIGHT=${SAVEDIR}/defrcn_det_r101_base/model_reset_remove.pth
-#for seed in 0 1 2 3 4 5 6 7 8 9
-for seed in 0
+for seed in 0 1 2 3 4 5 6 7 8 9
+#for seed in 0
 do
     for shot in 1 2 3 5 10 30
     do
